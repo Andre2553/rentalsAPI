03927e8a811616a7fc37358d1fd530f5db31102e
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-12-01 22:39:21 +1100

    Import Categories handler
    - import categories from csv files

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+
+import csvParse from "csv-parse";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+
+interface IImportCategory {
+   name: string;
+   description: string;
+}
+
+class ImportCategoryUseCase {
+   constructor(private categoriesRepository: ICategoriesRepository) { }
+
+   loadcategories(file: Express.Multer.File) : Promise<IImportCategory[]> {
+      return new Promise((resolve, reject) => {
+         const stream = fs.createReadStream(file.path);
+         const categories: IImportCategory[] = [];
+
+         const parseFile = csvParse.parse();
+         stream.pipe(parseFile);
+
+         parseFile.on("data", async (line) => {
+            const [name, description] = line;
+            categories.push({ name, description });
+         })
+            .on("end", () => {
+               resolve(categories);
+            })
+            .on("error", (err) => {
+               reject(err);
+            });
+      })
+   }
+
+   async execute(file: Express.Multer.File): Promise<void> {
+      const categories = await this.loadcategories(file);
+      
+      categories.map(async (category) => {
+         const { name, description } = category;
+
+         const existCategory = this.categoriesRepository.findByName(name);
+
+         if (!existCategory) {
+            this.categoriesRepository.create({ name, description });
+         }
+      });
+   }
+}
+
+export { ImportCategoryUseCase }
