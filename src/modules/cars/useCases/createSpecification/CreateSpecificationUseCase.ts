574033445286379aaf819f2c64de8d8b4310d981
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-12-01 19:38:03 +1100

    Refactoring the project

--- src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -1,15 +1,15 @@
-import { ICreateSpecificationDTO, ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
 
 interface IRequest{
    name: string;
    description: string;
 }
 
-class CreateSpecificationService {
+class CreateSpecificationUseCase {
    constructor(private specificationsRepository: ISpecificationsRepository) {
 
    }
-   execute({ name, description }: ICreateSpecificationDTO): void {
+   execute({ name, description }: IRequest): void {
       const specificationAlreadyExists = this.specificationsRepository.findByName(name);
 
       if (specificationAlreadyExists) {
@@ -20,4 +20,4 @@ class CreateSpecificationService {
    }
 }
 
-export {CreateSpecificationService}
+export {CreateSpecificationUseCase}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,9 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
 
-app.use("/categories", categoriesRoutes);
-app.use('/specifications', specificationRoutes);
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+app.use(router);
 
 app.listen(3333);
