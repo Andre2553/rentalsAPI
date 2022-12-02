eaa7a75e1a97c8e17e9dc109b381cade2428221d
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-12-02 11:57:06 +1100

    Docker init
    - dockerfile
    - docker-compose

--- src/modules/cars/repositories/implementations/PostgresCategoriesRepository.ts
+++ src/modules/cars/repositories/implementations/PostgresCategoriesRepository.ts
@@ -1,5 +1,5 @@
-import { Category } from "../model/Category";
-import { ICategoriesRepository } from "./ICategoriesRepository";
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../ICategoriesRepository";
 
 class PostgresCategoriesRepository implements ICategoriesRepository{
    findByName(name: string): Category {
